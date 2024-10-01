82987e9c00ce90e658a78fef9a2d8a4ebd2de1cc
Faiz-Ahimsa <24106050019@student.uin-suka.ac.id>
2024-10-01 09:27:23 +0700

    Create reamur_ke_fahrenheit.c

---
+++ reamur_ke_fahrenheit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main(){
+    int reamur, fahrenheit;
+
+    printf("input reamur: ");
+    scanf("%i", &reamur);
+
+    fahrenheit = reamur*2.25+32;
+    printf("suhu fahrenheit: %i", fahrenheit);
+}
